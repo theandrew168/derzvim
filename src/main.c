ef2220a1ae22e3e82011768c996a5c75a3c18a92
theandrew168 <steveday168@gmail.com>
2020-06-23 16:28:06 -0500

    Add comments for next steps

--- src/main.c
+++ src/main.c
@@ -10,13 +10,6 @@
 #include "array.h"
 #include "term.h"
 
-static long
-text_index()
-{
-
-    return -1;
-}
-
 int
 main(int argc, char* argv[])
 {
@@ -61,24 +54,30 @@ main(int argc, char* argv[])
     long scroll = 0;
     long cx = 0;
     long cy = 0;
+    long lx = 0;
+    long ly = 0;
+    // TODO: DDL of lines?
 
-    // capture the origina termios config to restore later
+    // capture the original termios config to restore later
     struct termios original_termios;
     if (tcgetattr(input_fd, &original_termios) == -1) {
         fprintf(stderr, "error capturing default termios: %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
 
+    // TODO: handle resizing via sigwinch and volatile sig_atomic_t?
     if (!term_size(output_fd, &width, &height)) {
         fprintf(stderr, "error getting term size: %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
 
+    // put the terminal into raw mode
     if (!term_mode_raw(input_fd)) {
         fprintf(stderr, "error enabling raw mode: %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
 
+    // initially, clear the screen and maybe draw some tildes?
     term_cursor_reset(output_fd);
     term_cursor_hide(output_fd);
     term_screen_clear(output_fd);
@@ -99,51 +98,49 @@ main(int argc, char* argv[])
 
         // process the input
         switch (c) {
+            // quit derzvim
             case CTRL_KEY('q'):
                 running = false;
                 break;
+            // move the cursor left but not past the left edge
             case KEY_ARROW_LEFT:
                 if (cx <= 0) break;
                 cx--;
                 break;
+            // move the cursor right but not past the right edge OR end of line
             case KEY_ARROW_RIGHT:
                 if (cx >= width - 1) break;
                 cx++;
                 break;
+            // move the cursor up but not past the top edge
             case KEY_ARROW_UP:
                 if (cy <= 0) break;
                 cy--;
                 break;
+            // move the cursor down but not past the bottom edge OR end of file
             case KEY_ARROW_DOWN:
                 if (cy >= height - 1) break;
                 cy++;
                 break;
-            case KEY_PAGE_UP:
-                cy = 0;
-                break;
-            case KEY_PAGE_DOWN:
-                cy = height - 1;
-                break;
-            case KEY_HOME:
-                cx = 0;
-                break;
-            case KEY_END:
-                cx = width - 1;
-                break;
+            // break the line at the current cursor pos
             case KEY_ENTER:
                 if (cy >= height - 1) break;
                 cy++;
                 break;
+            // delete the char behind the cursor if not at left edge AND collapse lines
             case KEY_BACKSPACE:
                 if (cx <= 0) break;
                 cx--;
                 break;
+            // insert a char at the current cursor pos (adjust line as necessary)
             default:
                 if (c < 32 || c > 126) break;
                 break;
         }
 
         // update the screen
+        // for each line [offset:offset + height]:
+        //  draw the line [0 to height] and handle wrapping (extra y++)
         term_cursor_reset(output_fd);
         term_cursor_hide(output_fd);
         term_screen_clear(output_fd);
