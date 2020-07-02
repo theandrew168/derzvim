330678e9f0149dca71a1e3e5d854fff6cbbf6e12
theandrew168 <steveday168@gmail.com>
2020-07-02 11:25:39 -0500

    Implement buggy line merging

--- src/editor.c
+++ src/editor.c
@@ -161,13 +161,13 @@ editor_run(struct editor* e)
             e->line_pos++;
             e->line_affinity = e->line_pos;
             break;
-        case KEY_ENTER:
+        case KEY_ENTER: {
             // break the current line
             line_break(e->line, e->line_pos);
             e->line_count++;
 
             // redraw original line
-            term_cursor_left(e->output_fd, e->line->size);
+            term_cursor_left(e->output_fd, 999);
             term_erase_line_after(e->output_fd);
             term_screen_write(e->output_fd, e->line->buf, e->line->size);
 
@@ -191,7 +191,57 @@ editor_run(struct editor* e)
 
             e->line_affinity = 0;
             e->line_pos = 0;
-            break;
+        }   break;
+        case KEY_BACKSPACE: {
+            // middle of a line
+            if (e->line_pos > 0) {
+                e->line_pos--;
+                e->line_affinity--;
+                line_delete(e->line, e->line_pos);
+                term_cursor_left(e->output_fd, 1);
+                term_cursor_save(e->output_fd);
+                term_cursor_left(e->output_fd, 999);
+                term_erase_line_after(e->output_fd);
+                term_screen_write(e->output_fd, e->line->buf, e->line->size);
+                term_cursor_restore(e->output_fd);
+                break;
+            }
+
+            // start of the first line
+            if (e->line->prev == NULL) break;
+
+            // start of any other line
+
+            long prev_size = e->line->prev->size;
+
+            // merge the two lines
+            e->line = e->line->prev;
+            e->line_pos = e->line->size;
+            e->line_affinity = e->line->size;
+            line_merge(e->line, e->line->next);
+            e->line_count--;
+
+            // scroll region up
+            long cx, cy;
+            term_cursor_pos_get(e->output_fd, e->input_fd, &cx, &cy);
+            term_cursor_save(e->output_fd);
+            term_scroll_region_set(e->output_fd, cy, e->width - 2);
+            term_cursor_down(e->output_fd, 999, false);
+            term_cursor_down(e->output_fd, 1, true);
+            // TODO: draw the new line that scrolled in ELSE clear
+            term_scroll_region_set(e->output_fd, 0, e->width - 2);
+            term_cursor_restore(e->output_fd);
+
+            // redraw merged line
+            term_cursor_up(e->output_fd, 1, true);
+            term_cursor_left(e->output_fd, 999);
+            term_erase_line_after(e->output_fd);
+            term_screen_write(e->output_fd, e->line->buf, e->line->size);
+
+            // set cursor to merge position
+            term_cursor_left(e->output_fd, 999);
+            term_cursor_right(e->output_fd, prev_size);
+        }   break;
         default:
             if (c < 32 || c > 126) break;
             line_insert(e->line, e->line_pos, c);
@@ -250,82 +300,6 @@ editor_run(struct editor* e)
 //    return EDITOR_OK;
 //}
 //int
-//editor_cursor_up(struct editor* e)
-//{
-//    assert(e != NULL);
-//
-//    // if at top of lines, done
-//    if (e->line->prev == NULL) return EDITOR_OK;
-//
-//    // vertical scrolling
-//    if (e->cursor_y <= 0) {
-//        e->scroll_y--;
-//    } else {
-//        e->cursor_y--;
-//    }
-//
-//    // move to the prev line
-//    e->line = e->line->prev;
-//    e->line_index--;
-//
-//    // handle affinity
-//    if (e->line_affinity >= e->line->size) {
-//        if (e->line->size >= e->width + e->scroll_x) e->scroll_x = e->line_affinity - e->width + 1;
-//        e->cursor_x = e->line->size - e->scroll_x;
-//        e->line_pos = e->line->size;
-//    } else {
-//        e->cursor_x = e->line_affinity - e->scroll_x;
-//        e->line_pos = e->line_affinity;
-//    }
-//
-//    // horizontal scrolling
-//    if (e->line_pos < e->scroll_x) {
-//        e->scroll_x = MAX(e->line_pos - e->width, 0);
-//        e->cursor_x = e->line_pos;
-//    }
-//
-//    return EDITOR_OK;
-//}
-//
-//int
-//editor_cursor_down(struct editor* e)
-//{
-//    assert(e != NULL);
-//
-//    // if at bottom of lines, done
-//    if (e->line->next == NULL) return EDITOR_OK;  
-//
-//    // vertical scrolling
-//    if (e->cursor_y >= e->height - 2) {
-//        e->scroll_y++;
-//    } else {
-//        e->cursor_y++;
-//    }
-//
-//    // move to the next line
-//    e->line = e->line->next;
-//    e->line_index++;
-//
-//    // handle affinity
-//    if (e->line_affinity >= e->line->size) {
-//        if (e->line->size >= e->width + e->scroll_x) e->scroll_x = e->line_affinity - e->width + 1;
-//        e->cursor_x = e->line->size - e->scroll_x;
-//        e->line_pos = e->line->size;
-//    } else {
-//        e->cursor_x = e->line_affinity - e->scroll_x;
-//        e->line_pos = e->line_affinity;
-//    }
-//
-//    // horizontal scrolling
-//    if (e->line_pos < e->scroll_x) {
-//        e->scroll_x = MAX(e->line_pos - e->width, 0);
-//        e->cursor_x = e->line_pos;
-//    }
-//
-//    return EDITOR_OK;
-//}
-//
-//int
 //editor_cursor_home(struct editor* e)
 //{
 //    assert(e != NULL);
@@ -350,30 +324,6 @@ editor_run(struct editor* e)
 //    e->cursor_x = MIN(size, e->width - 1);
 //    e->line_pos = size;
 //    e->line_affinity = size;
-//
-//    return EDITOR_OK;
-//}
-//
-//int
-//editor_cursor_page_up(struct editor* e)
-//{
-//    assert(e != NULL);
-//
-//    for (long i = 0; i < e->height - 1; i++) {
-//        editor_cursor_up(e);
-//    }
-//
-//    return EDITOR_OK;
-//}
-//
-//int
-//editor_cursor_page_down(struct editor* e)
-//{
-//    assert(e != NULL);
-//
-//    for (long i = 0; i < e->height - 1; i++) {
-//        editor_cursor_down(e);
-//    }
 //
 //    return EDITOR_OK;
 //}
